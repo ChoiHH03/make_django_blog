1701bb58c761d599438d4963d31270800226ce1c
ChoiHH <choihh0316@gmail.com>
2021-03-26 11:20:25 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/views.py
+++ blog/views.py
@@ -96,7 +96,6 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
             return response
 
-            return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
 
@@ -122,6 +121,27 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self, form):
+            response = super(PostUpdate, self).form_valid(form)
+            self.object.tags.clear()
+
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(',', ';')
+                tags_list = tags_str.split(';')
+
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
