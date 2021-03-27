922f896b1b2a67a291b5fd7a1cce6dd68bb8bb0e
ChoiHH <choihh0316@gmail.com>
2021-03-27 10:17:04 +0900

    댓글 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
-    path('update_post/<int:pk>/', views.PostUpdate.as_view())
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment)
     # path('',views.index),
     # path('<int:pk>/', views.single_post_page),
 ]
